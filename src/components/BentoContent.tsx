8f5e3824ad8477001dc3c46692d2b773f52f5d4f
Andres Castellanos <andres.about@gmail.com>
2024-04-05 11:30:14 -0500

    adding real content to projects

--- src/components/BentoContent.tsx
+++ src/components/BentoContent.tsx
@@ -18,18 +18,18 @@ interface Props {
 
 function BentoContent({ handleClose, project }: Props) {
   if (project !== null) {
-    const { priority: area } = project.data
+    const { priority: area, title, description } = project.data
     return (
       <section className={`relative w-full h-full card flex flex-col bg-background ${openAreas[area]} `}>
         <Button size={'icon'} className="absolute top-3 right-2" variant={"ghost"} onClick={handleClose}>
           <Exit className=" w-4 h-4 md:w-5 md:h-5 lg:w-6 lg:h-6" />
         </Button>
-        <h2 className={cn("title", contentConfig[area].title)}>Digital hippo</h2>
-        <p className={cn("base mt-2", contentConfig[area].desc)}>A Ecommerce website to Buy and Sell digital assets</p>
+        <h2 className={cn("title", contentConfig[area].title)}>{title}</h2>
+        <p className={cn("base mt-2", contentConfig[area].desc)}>{description}</p>
         <Accordion className={cn("mt-3 md:mt-6", contentConfig[area].accordion)} type="single" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger>Main Features</AccordionTrigger>
-            <AccordionContent>
+            <AccordionTrigger className="subtitle">Main Features</AccordionTrigger>
+            <AccordionContent className="base">
               <ul className="list">
                 {project.data.main_features.map(features => (
                   <li key={crypto.randomUUID()}>{features}</li>
@@ -38,21 +38,21 @@ function BentoContent({ handleClose, project }: Props) {
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
-            <AccordionTrigger>Challenges</AccordionTrigger>
-            <AccordionContent>
+            <AccordionTrigger className="subtitle">Challenges</AccordionTrigger>
+            <AccordionContent className="base">
               <ul>
                 {project.data.challenges.map(challenge => (
                   <li key={crypto.randomUUID()}>
-                    <h3 className="font-bold mt-2">{challenge.title}</h3>
-                    <p className="mt-1">{challenge.solution}</p>
+                    <h3 className="font-bold text-base md:text-lg lg:text-lg mt-2">{challenge.title}</h3>
+                    <p className="mt-1.5">{challenge.solution}</p>
                   </li>
                 ))}
               </ul>
             </AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-3">
-            <AccordionTrigger>What I learned</AccordionTrigger>
-            <AccordionContent>
+            <AccordionTrigger className="subtitle">What I learned</AccordionTrigger>
+            <AccordionContent className="base">
               <ul className="list">
                 {project.data.what_i_learned.map(item => (
                   <li key={crypto.randomUUID()}>{item}</li>
@@ -61,7 +61,7 @@ function BentoContent({ handleClose, project }: Props) {
             </AccordionContent>
           </AccordionItem>
         </Accordion>
-        <div className="mt-8 flex gap-2">
+        <div className="mt-auto flex gap-2">
           <a target="_blank" className={cn(buttonVariants({ variant: "default" }), "flex-grow")} href={project.data.demo}>
             Demo
           </a>
